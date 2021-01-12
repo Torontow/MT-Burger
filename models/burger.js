61473b16749eca8eb04727ee8b1eb6b1beded684
Torontow <mtorontow0@gmail.com>
2021-01-12 13:11:05 -0500

    Bug Fix and Function Added
    Fixed problem where orm was incorrectly required, and built the selectAll function.

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,18 @@
-const connection = require('connection');
-const orm = requre('orm');
+const orm = require('../config/orm.js');
 
 const burger = {
 
 // call the orm functions that will use user burger input
+
+// VIEW all burgers from database.
+
+    selectAll(cb) {
+        orm.selectAll('burgers', (res) => cb(res));
+    },
+
+// CREATE new burger orm function.
+
+// UPDATE burger status orm function.
 
 };
 
