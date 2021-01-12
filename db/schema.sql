7752bec6cd7aca333c12098f9ff393a0072e26e5
Torontow <mtorontow0@gmail.com>
2021-01-11 22:44:25 -0500

    Bug Fix
    Had extraneous instance of the word 'values' in schema.

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ create database burgers_db;
 
 use database burgers_db;
 
-create table burgers values (
+create table burgers (
     id int not null auto_increment,
     burger_name varchar(40),
     devoured tinyint(0)
