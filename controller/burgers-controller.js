88a1e4efa8f16fb86eed10eb29df16baf093d71d
Torontow <mtorontow0@gmail.com>
2021-01-12 09:03:15 -0500

    Controller Added
    Added burger-controller file within controller folder which will handle the routes for the application.

---
+++ controller/burgers-controller.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const burger = require('../models/burger.js');
+
+
+// Get Router
+
+// Post Router
+
+// Update Router
+
+
+
+module.exports = router;
