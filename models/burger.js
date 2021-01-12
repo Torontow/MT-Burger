bce0062bec19fc89420ffe37c6ea82616479dea2
Torontow <mtorontow0@gmail.com>
2021-01-12 08:56:10 -0500

    Model Folder Added
    Added burger.js within a model folder for using orm methods.

---
+++ models/burger.js
@@ -0,0 +1,11 @@
+const connection = require('connection');
+const orm = requre('orm');
+
+const burger = {
+
+// call the orm functions that will use user burger input
+
+};
+
+
+module.exports = burger;
