bc5ea740425b5a38f294e6fcfcf6e12955363f4a
Torontow <mtorontow0@gmail.com>
2021-01-14 18:45:18 -0500

    Built Put
    Update funtionality added.

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -1,29 +1,49 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const burger = require('../models/burger.js');
+const burger = require('../models/burger.js')
+
+
+router.put('api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`
+
+  console.log('condition', condition)
+
+  burger.updateOne(
+    {
+      devoured: req.params.devoured
+    },
+    condition,
+    result => {
+      if (result.changedRows === 0) {
+        return res.status(404).end()
+      }
+      res.status(200).end()
+    }
+  )
+})
 
 // Router to VIEW all burgers.
 
 router.get('/', (req, res) => {
-    burger.selectAll((data) => {
-        const hbsObject = {
-            burgers: data,
-        };
-        console.log('hbsObject', hbsObject);
-        res.render('index', hbsObject);
-    });
-});
+  burger.selectAll(data => {
+    const hbsObject = {
+      burgers: data
+    }
+    console.log('hbsObject', hbsObject)
+    res.render('index', hbsObject)
+  })
+})
 
 // Post Router
 
 router.post('/api/burgers', (req, res) => {
-    burger.insertOne('burger_name', req.body.name, (result) => {
-        res.json({ id: result.insertId });
-    })
+  burger.insertOne(['burger_name','devoured'], [req.body.name,req.body.devoured], (result) => {
+    res.json({ id: result.insertId })
+  })
 })
+
 // Update Router
 
 
-
-module.exports = router;
+module.exports = router
