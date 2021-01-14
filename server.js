563dcfdca6734300db232be35d143df11909aa17
Torontow <mtorontow0@gmail.com>
2021-01-14 18:50:28 -0500

    Bug Fix
    Worked on some changes in spacing.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
 const routes = require('./controllers/burgers-controller.js');
-
+  
 app.use(routes);
 
 app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
