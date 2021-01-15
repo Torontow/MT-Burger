0224503a65f07259ffe827a49b7aa29942123ac5
Torontow <mtorontow0@gmail.com>
2021-01-14 21:51:03 -0500

    Heroku
    Added heroku deployment info.

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ const connection = mysql.createConnection({
   database: 'burgers_db',
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Xavier1977!',
+    database: 'burgers_db'
+  })
+}
+
 // Make connection.
 connection.connect((err) => {
   if (err) {
