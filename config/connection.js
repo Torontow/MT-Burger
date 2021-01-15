77f64dc058b995783af8f3a1a2c3044e30bb7702
Torontow <mtorontow0@gmail.com>
2021-01-14 22:14:10 -0500

    Heroku bug fix

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'Xavier1977!',
-  database: 'burgers_db',
-});
+// let connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   // NOTE: Be sure to add your MySQL password here!
+//   password: 'Xavier1977!',
+//   database: 'burgers_db',
+// });
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
